ae876ab3365f48435a95b2efa657b5de5490508f
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2025-05-08 19:36:43 -0300

    Tipo de dados
    Tipo de dados

---
+++ 20225.05.08_introducaoJavaContinuacao/app02.js
@@ -0,0 +1,69 @@
+// Boas práticas
+// Case sensitive
+// Sintaxe básica e comentário
+// Declaração de variáveis(var, let, const)
+
+const nome1 = "Emerson" //palavra reservada
+
+let nome = "Emerson1" // Para declar let ou var.
+let idade = 36 
+
+// Console e Debug
+console.log(nome)
+console.log(idade)
+
+nome = "Eduardo"
+idade = "12"
+
+console.log(nome, idade)
+
+
+//2025.05.08 - Continuação
+
+//Tipod de dados
+
+// text == strig
+let cidade = "americana"
+
+// numerico == number
+let salario = 1500.35
+
+//boleano == boolean - Verdade ou falso
+let fumante = false
+
+console.log(typeof cidade)
+console.log(typeof idade)
+console.log(typeof salario)
+
+// Operadores
+//     Operador Atribuição
+//     Operadores aritméticos
+//     Operadores relacionais
+//     Operadores lógicos
+
+// Estrutura
+//     Estrutura de controle/decisão
+//     Laços de repetição
+
+// Arrays 
+
+// Funções básicas
+// Escopo
+
+
+// Alterar conteúdo e atributos *
+// incluir js em página html *
+// Eventos *
+// DOM *
+// Objeto
+// parâmetros e retorno*
+
+// Programação assíncrona***
+// API Fetch ***
+
+// JSON ****
+// funções modernas (arrow functions) ****
+
+
+// Spread e REST  ************
+ 
