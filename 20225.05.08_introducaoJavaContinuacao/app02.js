b095c874ac30362b5a9e026b184bdd90b1f37538
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2025-05-08 21:31:25 -0300

    Operadores lógicos  + Desafio
    Operadores lógicos + Desafio

--- 20225.05.08_introducaoJavaContinuacao/app02.js
+++ 20225.05.08_introducaoJavaContinuacao/app02.js
@@ -34,12 +34,52 @@ let fumante = false
 console.log(typeof cidade)
 console.log(typeof idade)
 console.log(typeof salario)
+console.log()
 
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
-//     Operadores lógicos
+//     Operador Atribuição (=)
+//     Operadores aritméticos (+, -, /, *)
+console.log(10+15)
+console.log("10+15")// Se colocar " " ele vira texto que não faz os contas.
+let n1 = 10
+let n2 = 5
+console.log(n1 + n2) // soma
+console.log(n1 - n2) // subtração
+console.log(n1 / n2) // divisão
+console.log(n1 * n2) // mutiplicação
+console.log(n1 % 3)  // módulo (resto da divisão)
+console.log()
+
+//     Operadores relacionais (>, <, >=, <=, !=, ==)
+console.log(n1 == n2)   // igualdade
+console.log(n1 != n2)   // diferente
+console.log(n1 > n2)    // maior
+console.log(10 < 10)    // menor
+console.log(10 <= 10)   // menor igual
+console.log(150 >= 175) // maior igual
+console.log()
+
+//     Operadores lógicos (! "não", && "e", || "ou")
+console.log(!10 > 2) // "!" Não - inverte o resultado, se for verdadeiro muda para falso, se for falso muda para verdade "negação"
+console.log(!false)
+console.log(10 > 2 && 35 < 100 && n1 > n2 && 100 < 90) // "&&" E - Todas as verificações precisam ser verdadeiras para o resultado ser Verdadeiro
+console.log(10 < 2 || 100 == 150 || 57 == 57) // "||" OU - apenas uma verificação precisa ser verdadeira para o resultado ser verdadeiro, o resultado só será falso quando TODAS as verificações forem falsas
+console.log()
+
+//DESAFIO
+let preco = 100
+let precoAcrescimo = 0
+let precoDesconto = 0
+//faça um código que acresente 17% ao preço e imprima
+precoAcrescimo = ((preco * 17)/100 + preco)
+console.log("Preço com Acrescimo: " + precoAcrescimo)
+//faça um código que desconte 7% do preço e imprima
+precoDesconto = (preco - ((preco * 7)/100))
+console.log("Preço com Desconto: " + precoDesconto)
+
+precoDesconto = preco - preco * 0.07
+console.log("Preço com desconto: " + precoDesconto.toFixed(2))
+
 
 // Estrutura
 //     Estrutura de controle/decisão
