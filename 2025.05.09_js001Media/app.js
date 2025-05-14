e6f14a1a2ad614af9bf75d17a7a2506c812dcf15
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2025-05-13 22:08:43 -0300

    Finalização da calculadora de media
    Finalização da calculadora de media

--- 2025.05.09_js001Media/app.js
+++ 2025.05.09_js001Media/app.js
@@ -15,7 +15,47 @@ function calcularMedia(){
 
     //Atualiza o campo da média
     document.getElementById('media').value = media.toFixed(2);
+
+     //Mostra se foi aprovado ou reprovado
+    // if(media >= 5){
+    //     document.getElementById('resultado').value = "Aprovado";
+    // }
+    // else{
+    //     document.getElementById('resultado').value = "Reprovado";
+    // }
+
+    const resultado = document.getElementById('resultado');
+
+    if (media >= 5) {
+        resultado.value = "Aprovado";
+        resultado.classList.remove('reprovado');
+        resultado.classList.add('aprovado');
+    } else {
+        resultado.value = "Reprovado";
+        resultado.classList.remove('aprovado');
+        resultado.classList.add('reprovado');
+    }
 }
 
 
+function calcularCelcius(){
+    //para pegar os valores farenheit
+    const farenheit = parseFloat(document.getElementById('farenheit').value) || 0;
+
+    //Calcular o Celcios
+    const celcius = ((farenheit - 32)*(5/9));
+
+    //Atualiza o campo
+    document.getElementById('celcius').value = celcius.toFixed(2) + '°';
+
+    //para pegar os valores Celcius
+    const celcius1 = parseFloat(document.getElementById('celcius1').value) || 0;
+
+    //Calcular o Celcios
+    const farenheit1 = (celcius1 * (9/5))+32;
+
+    //Atualiza o campo
+    document.getElementById('farenheit1').value = farenheit1.toFixed(2) + '°';
+
+}
 
