0e554c1ecf02c0642c3247d4b3301ee0b4aa164c
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2025-07-03 22:25:11 -0300

    Criação do Card
    Criação do Card

--- 2025.07.03_react-rick-and-morty/src/App.jsx
+++ 2025.07.03_react-rick-and-morty/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 
 function App() {
-  
+
   return (
     <>
-      <h1>Ola mundo</h1>
+      <div className='card'>
+        <h1 className='card-title'>Rick Sanchez</h1>
+        <span className='tag'>Status: Vivo</span>
+        <span className='tag'>Espécie: Humana</span>
+        <span className='tag'>Origem: Terra C-137</span>
+        <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg' className='card-image' />
+      </div>
     </>
   )
 }
