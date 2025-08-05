904fb67db932d5c816b2b9e76a21827b346d6510
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2025-08-05 20:17:51 -0300

    feat: criação da página título
    feat: criação da página título

--- 2025.08.05_react_dollar/src/App.jsx
+++ 2025.08.05_react_dollar/src/App.jsx
@@ -1,14 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Titulo from './Titulo';
 
 function App() {
   
   return (
     <>
-      <h1 className='display-3'>Calculadora Dólar</h1>
+      <div className='conteiner'>
+        <Titulo />
+      </div>
+      {/* <h1 className='display-3'>Calculadora Dólar</h1>
       <img src="/public/dollar.gif" width='50' />
       <br />
-      <button className='btn btn-primary'>Click Aqui</button>
+      <button className='btn btn-primary'>Click Aqui</button> */}
     </>
   )
 }
